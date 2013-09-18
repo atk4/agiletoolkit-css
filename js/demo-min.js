7e131085daa0958bb734d920bb56e6b4ab0aede2
Mayack <m@agiletech.ie>
2013-09-18 20:41:36 +0100

    Started integrating jQueryUI

--- js/demo-min.js
+++ js/demo-min.js
@@ -1,1 +1,1 @@
-$(function(){});
+$(function(){$("#datepicker").datepicker();$("#tabs").tabs();$("#accordion").accordion({heightStyle:"content"});$("#dialog").dialog({autoOpen:!1,modal:!0});$("#dialog-minimal").dialog({autoOpen:!1,modal:!0,dialogClass:"atk-dialog-minimal"});$("#opener").click(function(){$("#dialog").dialog("open")});$("#opener2").click(function(){$("#dialog-minimal").dialog("open")})});
--- js/demo.js
+++ js/demo.js
@@ -1,5 +1,22 @@
 $(function(){
-
-
-
+	$( "#datepicker" ).datepicker();
+	$( "#tabs" ).tabs();
+	$( "#accordion" ).accordion({
+		heightStyle: "content"
+	});
+  $( "#dialog" ).dialog({
+  	autoOpen: false,
+  	modal: true
+  });
+  $( "#dialog-minimal" ).dialog({
+  	autoOpen: false,
+  	modal: true,
+  	dialogClass: "atk-dialog-minimal"
+  });
+    $( "#opener" ).click(function() {
+    	$('#dialog').dialog("open");
+    });
+    $( "#opener2" ).click(function() {
+    	$('#dialog-minimal').dialog("open");
+    });
 });
