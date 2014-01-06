2ec79b1b703a510f8a9acc32a99f133faf91d443
Romans Malinovskis <r@agiletech.ie>
2014-01-06 04:09:12 +0000

    Customize dialog to use <a> for buttons.

--- playground/js/demo.js
+++ playground/js/demo.js
@@ -1,3 +1,48 @@
+$.widget( "ui.dialog", $.ui.dialog, {
+    _createButtons: function() {
+            var that = this,
+                    buttons = this.options.buttons;
+
+            // if we already have a button pane, remove it
+            this.uiDialogButtonPane.remove();
+            this.uiButtonSet.empty();
+
+            if ( $.isEmptyObject( buttons ) || ($.isArray( buttons ) && !buttons.length) ) {
+                    this.uiDialog.removeClass( "ui-dialog-buttons" );
+                    return;
+            }
+
+            $.each( buttons, function( name, props ) {
+                    var click, buttonOptions;
+                    props = $.isFunction( props ) ?
+                            { click: props, text: name } :
+                            props;
+                    // Default to a non-submitting button
+                    props = $.extend( { type: "button" }, props );
+                    // Change the context for the click callback to be the main element
+                    click = props.click;
+                    props.click = function() {
+                            click.apply( that.element[ 0 ], arguments );
+                    };
+                    buttonOptions = {
+                            icons: props.icons,
+                            text: props.showText
+                    };
+                    delete props.icons;
+                    delete props.showText;
+                    $( "<a></a>", props )
+                            .button( buttonOptions )
+                            .appendTo( that.uiButtonSet );
+            });
+            this.uiDialog.addClass( "ui-dialog-buttons" );
+            this.uiDialogButtonPane.appendTo( this.uiDialog );
+    }
+});
+ 
+$( "<div>" ).dialog();
+
+
+
 $(function(){
 
     $('.do-theme-switch').children().click(function(){
