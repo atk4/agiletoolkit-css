db8a19ba4c64ba47fa024e6b60d00b118a4a2040
Mayack <m@agiletech.ie>
2014-02-12 09:12:20 +0200

    Adjusted typography modular scale. Added new classes for .atk-table widget.

--- playground/js/demo.js
+++ playground/js/demo.js
@@ -1,19 +1,39 @@
 $(function(){
 
-    $('.do-theme-switch').children().click(function(){
-        $('link').attr('href','css/'+$(this).data('file')+'?'+(Math.random()*10000));
-    });
+  // Theme Switch
+  $('.do-theme-switch').children().click(function(){
+      $('link').attr('href','css/'+$(this).data('file')+'?'+(Math.random()*10000));
+  });
 
+  // Icons
+  $('#icons-library').hide();
+  $('#example-icons').click(
+    function(){
+      $('#icons-library').fadeIn();
+      $('#toggler').fadeOut();
+      $(this).removeClass('atk-inline');
+      $(this).parent().removeClass('atk-inline');
+    }
+  );
+
+  // Select
   $('select').selectmenu();
+
+  // Datepicker
 	$("#datepicker").datepicker();
+
+  // Tabs
 	$("#tabs").tabs();
   $("#tabs2").tabs();
   $("#tabs3").tabs();
   $("#tabs4").tabs();
-  $('#icons-library').hide();
+
+  // Accordion
 	$("#accordion").accordion({
 		heightStyle: "content"
 	});
+
+  // Dialog
   $( "#dialog" ).dialog({
   	autoOpen: false,
   	modal: true
@@ -46,12 +66,20 @@ $(function(){
     $( "#opener3" ).click(function() {
       $('#dialog-menu').dialog("open");
     });
-    $('#example-icons').click(
-      function(){
-        $('#icons-library').fadeIn();
-        $('#toggler').fadeOut();
-        $(this).removeClass('atk-inline');
-        $(this).parent().removeClass('atk-inline');
+
+    // Expander
+    var x;
+    $('#table-expander .atk-table tbody tr:not(".atk-table-expander")').click(function(){
+      if ( x != "open" ) {
+        $(this).next('.atk-table-expander').show();
+        $(this).find('.icon-right-dir').removeClass('icon-right-dir').addClass('icon-down-dir');
+        x = "open";
+      } else {
+        $(this).next('.atk-table-expander').hide();
+        $(this).find('.icon-down-dir').removeClass('icon-down-dir').addClass('icon-right-dir');
+        x = "close";
       }
-    );
+
+    });
+
 });
